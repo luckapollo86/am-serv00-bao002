ea2b99fde2848f1918badbfd63504dfaa7244ff4
luckapollo86 <151613608+luckapollo86@users.noreply.github.com>
2024-11-21 11:42:57 +0800

    Update keep_serv00.sh

--- keep_serv00.sh
+++ keep_serv00.sh
@@ -7,5 +7,10 @@ red="\033[31m"
 purple() { echo -e "\033[35m$1\033[0m"; }
 re="\033[0m"
 
-# 执行脚本
-/home/foot9/.vmess/s9.sh
+# 获取脚本 URL
+get_script_url() {
+    case $1 in
+        vmess) echo "${base_url}/foot9/.vmess/s9.sh" ;;
+  		  # x-ui) echo "${base_url}/am-serv00-x-ui/main/am_restart_x_ui.sh" ;;
+       esac
+}
